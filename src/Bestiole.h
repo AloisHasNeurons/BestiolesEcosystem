e18f265e03ed79d757852dd5648dc51933c3ae61
Alois <alois.vincent@imt-atlantique.net>
2025-11-14 18:39:08 +0000

    chore: linted Google-style

--- src/Bestiole.h
+++ src/Bestiole.h
@@ -1,54 +1,47 @@
 #ifndef _BESTIOLES_H_
 #define _BESTIOLES_H_
 
+#include <iostream>
 
 #include "UImg.h"
 
-#include <iostream>
-
 using namespace std;
-
 
 class Milieu;
 
+class Bestiole {
+ private:
+  static const double AFF_SIZE;
+  static const double MAX_VITESSE;
+  static const double LIMITE_VUE;
 
-class Bestiole
-{
+  static int next;
 
-private :
-   static const double     AFF_SIZE;
-   static const double     MAX_VITESSE;
-   static const double     LIMITE_VUE;
+ private:
+  int identite;
+  int x, y;
+  double cumulX, cumulY;
+  double orientation;
+  double vitesse;
 
-   static int              next;
+  T* couleur;
 
-private :
-   int               identite;
-   int               x, y;
-   double            cumulX, cumulY;
-   double            orientation;
-   double            vitesse;
+ private:
+  void bouge(int xLim, int yLim);
 
-   T               * couleur;
+ public:                        // Forme canonique :
+  Bestiole(void);               // Constructeur par defaut
+  Bestiole(const Bestiole& b);  // Constructeur de copies
+  ~Bestiole(void);              // Destructeur
+                                // Operateur d'affectation binaire par defaut
+  void action(Milieu& monMilieu);
+  void draw(UImg& support);
 
-private :
-   void bouge( int xLim, int yLim );
+  bool jeTeVois(const Bestiole& b) const;
 
-public :                                           // Forme canonique :
-   Bestiole( void );                               // Constructeur par defaut
-   Bestiole( const Bestiole & b );                 // Constructeur de copies
-   ~Bestiole( void );                              // Destructeur
-                                                   // Operateur d'affectation binaire par defaut
-   void action( Milieu & monMilieu );
-   void draw( UImg & support );
+  void initCoords(int xLim, int yLim);
 
-   bool jeTeVois( const Bestiole & b ) const;
-
-   void initCoords( int xLim, int yLim );
-
-   friend bool operator==( const Bestiole & b1, const Bestiole & b2 );
-
+  friend bool operator==(const Bestiole& b1, const Bestiole& b2);
 };
-
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,16 @@
-#include "Aquarium.h"
-#include "Milieu.h"
-#include "Bestiole.h"
-
 #include <iostream>
+
+#include "Aquarium.h"
+#include "Bestiole.h"
+#include "Milieu.h"
 
 using namespace std;
 
+int main() {
+  Aquarium ecosysteme(640, 480, 30);
 
-int main()
-{
+  for (int i = 1; i <= 20; ++i) ecosysteme.getMilieu().addMember(Bestiole());
+  ecosysteme.run();
 
-   Aquarium       ecosysteme( 640, 480, 30 );
-
-   for ( int i = 1; i <= 20; ++i )
-      ecosysteme.getMilieu().addMember( Bestiole() );
-   ecosysteme.run();
-
-
-   return 0;
-
+  return 0;
 }
