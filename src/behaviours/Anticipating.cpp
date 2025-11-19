4b50edf9f6da800c490b69d593c1b88c38c77348
sadok <sadok.lajmi@imt-atlantique.net>
2025-11-19 23:16:40 +0100

    avancement sur behaviours (classes terminées)

---
+++ src/behaviours/Anticipating.cpp
@@ -0,0 +1,38 @@
+#include "behaviours/Anticipating.h"
+#include "interfaces/IBestiole.h"
+#include <cmath>
+
+double Anticipating::steer(IBestiole& b, std::vector<IBestiole*> bestiolesList) {
+  double closest_distance = std::numeric_limits<double>::max();
+  IBestiole* closest_bestiole = nullptr;
+  // search for the closest bestiole
+  for (std::vector<IBestiole*>::const_iterator it = bestiolesList.begin();
+       it != bestiolesList.end(); ++it) {
+    IBestiole* autre = (*it);
+    if (&b != autre && b.canSee(*autre)) {
+      double distance = std::sqrt((b.getX() - autre->getX()) * (b.getX() - autre->getX()) +
+                                  (b.getY() - autre->getY()) * (b.getY() - autre->getY()));
+      if (distance < closest_distance) {
+        closest_distance = distance;
+        closest_bestiole = autre;
+      }
+    }
+  }
+  // estimate future position of the closest bestiole
+  double future_y = -sin(closest_bestiole->getOrientation()) * (closest_bestiole->getSpeed()) + closest_bestiole->getY();
+  double future_x = cos(closest_bestiole->getOrientation()) * (closest_bestiole->getSpeed()) + closest_bestiole->getX();
+  // estimate future position of the current bestiole
+  double b_future_y = -sin(b.getOrientation()) * (b.getSpeed()) + b.getY();
+  double b_future_x = cos(b.getOrientation()) * (b.getSpeed()) + b.getX();
+  // compute the future distance
+  double future_distance = std::sqrt((b_future_x - future_x) * (b_future_x - future_x) +
+                                     (b_future_y - future_y) * (b_future_y - future_y));
+  // compute the anticipated orientation depending on this evolution of the distance
+  double anticipated_orientation;
+  if (future_distance < closest_distance) {
+    anticipated_orientation = closest_bestiole->getOrientation() + M_PI;  // avoid potential collision by passing parallel to this bestiole
+  } else {
+    anticipated_orientation = b.getOrientation();  // keep current orientation
+  }
+  return anticipated_orientation;
+}
---
+++ src/behaviours/Kamikaze.cpp
@@ -0,0 +1,30 @@
+#include "behaviours/Kamikaze.h"
+#include "interfaces/IBestiole.h"
+#include <limits>
+#include <cmath>
+
+double Kamikaze::steer(IBestiole& b, std::vector<IBestiole*> bestiolesList) {
+  double closest_distance = std::numeric_limits<double>::max();
+  IBestiole* closest_bestiole = nullptr;
+  // search for the closest bestiole
+  for (std::vector<IBestiole*>::const_iterator it = bestiolesList.begin();
+       it != bestiolesList.end(); ++it) {
+    IBestiole* autre = (*it);
+    if (&b != autre && b.canSee(*autre)) {
+      double distance = std::sqrt((b.getX() - autre->getX()) * (b.getX() - autre->getX()) +
+                                  (b.getY() - autre->getY()) * (b.getY() - autre->getY()));
+      if (distance < closest_distance) {
+        closest_distance = distance;
+        closest_bestiole = autre;
+      }
+    }
+  }
+
+  if (closest_bestiole) {
+    double adjusted_orientation = std::atan2(-(closest_bestiole->getY() - b.getY()),
+                                             closest_bestiole->getX() - b.getX());
+    return adjusted_orientation;
+  } else {
+    return b.getOrientation();
+  }
+}
