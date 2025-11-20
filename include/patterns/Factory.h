9d44e6f10c55d9875a060441d30216db40f37778
Alois <alois.vincent@imt-atlantique.net>
2025-11-20 18:06:36 +0000

    Fix include path in Factory.h.

--- include/patterns/Factory.h
+++ include/patterns/Factory.h
@@ -2,17 +2,16 @@
 #define FACTORY_H
 
 #include "IFactory.h"
-#include "Environment.h"
+#include "core/Environment.h"
 
 class Factory : public IFactory {
  private:
   const Environment& env; // Reference to the Environment that may influence Bestiole creation
-  
+
  public:
   Factory(const Environment& env); // Constructor with Environment reference
   IBestiole* createBestiole() override; // Method to create a new Bestiole
 
- 
 };
 
 #endif  // FACTORY_H
