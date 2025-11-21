05216c017cea25573c386a746e17c5b145ca20d9
Alois <alois.vincent@imt-atlantique.net>
2025-11-21 13:46:44 +0000

    Revert "Updated Gregarious and translated autre to other."
    This reverts commit 801f5e07838abb026cab152309100294feab7628.

--- src/behaviors/Gregarious.cpp
+++ src/behaviors/Gregarious.cpp
@@ -1,21 +1,34 @@
 #include "behaviors/Gregarious.h"
+
 #include "interfaces/IBestiole.h"
 
 double Gregarious::steer(IBestiole& b, std::vector<IBestiole*> bestiolesList) {
-  std::vector<double> orientations;
+  std::vector<double> positionsX, positionsY; // Positions of visible bestioles
   for (std::vector<IBestiole*>::const_iterator it = bestiolesList.begin();
        it != bestiolesList.end(); ++it) {
-    IBestiole* other = (*it);
-    if (b.canSee(*other) && &b != other) {
-    orientations.push_back(other->getOrientation());
-    }}
-  double sum = 0.0;
-  for (double ori : orientations) {
-    sum += ori;
+    IBestiole* autre = (*it);
+    if (b.canSee(*autre) && &b != autre) { // If b can see autre and they are not the same bestiole
+      positionsX.push_back(autre->getX());
+      positionsY.push_back(autre->getY());
+    } 
   }
-  if (orientations.empty()) {
-    return b.getOrientation();
+  double sumX = 0.0; // Sum of X positions
+  double sumY = 0.0; // Sum of Y positions
+  for (size_t i = 0; i < positionsX.size(); ++i) {
+    sumX += positionsX[i];
+    sumY += positionsY[i];
   }
-  double average_orientation = sum / orientations.size();
-  return average_orientation;
+  if (positionsX.empty()) {
+    return b.getOrientation(); // No visible bestioles, maintain current orientation
+  }
+  double avgX = sumX / positionsX.size();
+  double avgY = sumY / positionsY.size();
+  // Angle towards average position
+  double desiredOrientation = std::atan2(avgY - b.getY(), avgX - b.getX());
+  return desiredOrientation;
+}
+
+// [Fix] Implementation added to satisfy linker
+double Gregarious::speed(IBestiole& b, std::vector<IBestiole*> bestiolesList) {
+  return b.getSpeed();
 }
