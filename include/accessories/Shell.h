91687f49f1a27b132e756ce67179f4128da58973
boyang zhang <boyang.zhang@imt-atlantique.net>
2025-11-28 10:29:38 +0100

    change Shell.h

--- include/accessories/Shell.h
+++ include/accessories/Shell.h
@@ -7,7 +7,6 @@ class Shell : public IAccessory {
 public:
     Shell(IBestiole* b, double _omega, double _teta);
     ~Shell() override = default;
-    void draw(UImg& img) override;
 
     void action(Environment& env) override;
     void draw(UImg& img) override;
